6fc09ab7463f6ba0878d153ffbf1b4c21c0dc122
rzfeeser <rzfeeser@example.com>
2022-03-07 15:08:03 +0000

    Our first commit

---
+++ shebang.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Alta3 Research
+
+   Contact:
+   RZFeeser@alta3.com
+
+   Purpose:
+   learning about best practice, comments, git and shebang'''
+
+
+# display to standard out
+print("Hello team!") # print() is a standard library function
+
+
