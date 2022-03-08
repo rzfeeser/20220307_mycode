a8721147ca28f243bb116c81cdd4037c8bae6ba3
rzfeeser <rzfeeser@example.com>
2022-03-08 20:41:27 +0000

    end of day 2

---
+++ dellemc/unisphere_for_powermax_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser@alta3.com
+   Using DellEMC PyU4V client for management of PowerMax Storage Arrays via RESTful API requests to Unisphere.
+
+   PyU4V - Client for PowerMax Storage Array
+   GitHub @ https://github.com/dell/PyU4V/
+
+   Per GitHub README.MD:
+   PyU4V is a Python module that simplifies interaction with the Unisphere for PowerMax REST API. It wraps REST calls with simple APIs that abstract the HTTP request and response handling.
+
+   Requirements:
+   - Install PyU4V REST client:
+     python3 -m pip install PyU4V
+"""
+
+def main():
+    """run-time"""
+
+if __name__ == "__main__":
+    main()
