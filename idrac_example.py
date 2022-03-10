38827e89b053ba679db1d147b34203f5cf49c6e8
rzfeeser <rzfeeser@example.com>
2022-03-10 20:45:04 +0000

    end of day 4

--- idrac_example.py
+++ idrac_example.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python3
+"""Alta3 Research | RZFeeser@alta3.com
+   Making API request to iDrac interface"""
 
 import argparse # accept flags from CLI
-
 import requests # make HTTP lookup
 
-def token_getter(ip, username, password):
+def token_getter(ipaddr, username, password):
+    """returns a token"""
     # create the "finished" API to be queried
-    token_api = f'https://{ip}/redfish/v1/SessionService/Sessions'
+    token_api = f'https://{ipaddr}/redfish/v1/SessionService/Sessions'
     # make our API request
-    r = requests.post(token_api, json={'UserName': username, 'Password': password})
-    return r.headers['X-Auth token']
+    resp = requests.post(token_api, json={'UserName': username, \
+            'Password': password})
+    return resp.headers['X-Auth token']
 
 
 
 def main():
     """run time code"""
-
     token = token_getter(args.i, args.u, args.p)
-    
-
+    print(token)
 
 if __name__ == "__main__":
     # start to declare flag logic
     parser = argparse.ArgumentParser(description='Interact with iDRAC')
-    
+
     # user may set UserName
-    parser.add_argument('-u', metavar="IDRACUSER", type=str, default='root', help='Defines the iDRAC username')
+    parser.add_argument('-u', metavar="IDRACUSER", type=str, default='root',\
+            help='Defines the iDRAC username')
 
     # user may set Password
-    parser.add_argument('-p', metavar="IDRACPW", type=str, default='qwerty', help='Defines the iDRAC password')
+    parser.add_argument('-p', metavar="IDRACPW", type=str, default='qwerty',\
+            help='Defines the iDRAC password')
 
     # user may set the IP of the IDRAC to be contacted
-    parser.add_argument('-i', metavar="IDRACIP", type=str, help='IP address of the iDRAC')
-    
+    parser.add_argument('-i', metavar="IDRACIP", type=str,\
+            help='IP address of the iDRAC')
+
     # creates an args object
     args = parser.parse_args()
     main()
